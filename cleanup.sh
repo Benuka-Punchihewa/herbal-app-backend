91e6c1fd27a22daf295e06d2785f04a8b18e3102
Benuka Punchihewa <benuka1999@gmail.com>
2023-04-12 21:38:26 +0530

    updated to cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -11,7 +11,7 @@ kubectl delete -n default deployment api-gateway-deployment
 echo "Removing Kubernetes Services"
 kubectl delete -n default service auth-service
 kubectl delete -n default service user-service
-kubectl delete -n default service api-gateway
+kubectl delete -n default service api-gateway-service
 
 echo "Removing Kubernetes ConfigMaps"
 kubectl delete -n default configmap auth-config
@@ -19,6 +19,6 @@ kubectl delete -n default configmap user-config
 kubectl delete -n default configmap api-gateway-config
 
 echo "Removing Docker Images"
-docker rmi auth-service:1.0
-docker rmi user-service:1.0
-docker rmi api-gateway:1.0
+docker rmi -f auth-service:1.0
+docker rmi -f user-service:1.0
+docker rmi -f api-gateway:1.0

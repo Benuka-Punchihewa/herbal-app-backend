6ce76e1303eb864a247fc803d544500b93c4dbd9
Benuka Punchihewa <benuka1999@gmail.com>
2023-04-16 21:01:04 +0530

    payment service init
    configured docker & kubernetes

--- cleanup.sh
+++ cleanup.sh
@@ -9,6 +9,7 @@ kubectl delete -n default deployment user-deployment
 kubectl delete -n default deployment api-gateway-deployment
 kubectl delete -n default deployment product-deployment
 kubectl delete -n default deployment order-deployment
+kubectl delete -n default deployment payment-deployment
 
 echo "Removing Kubernetes Services"
 kubectl delete -n default service auth-service
@@ -16,6 +17,7 @@ kubectl delete -n default service user-service
 kubectl delete -n default service api-gateway-service
 kubectl delete -n default service product-service
 kubectl delete -n default service order-service
+kubectl delete -n default service payment-service
 
 echo "Removing Kubernetes ConfigMaps"
 kubectl delete -n default configmap auth-config
@@ -23,6 +25,7 @@ kubectl delete -n default configmap user-config
 kubectl delete -n default configmap api-gateway-config
 kubectl delete -n default configmap product-config
 kubectl delete -n default configmap order-config
+kubectl delete -n default configmap payment-config
 
 echo "Removing Docker Images"
 docker rmi -f auth-service:1.0
@@ -30,3 +33,4 @@ docker rmi -f user-service:1.0
 docker rmi -f api-gateway:1.0
 docker rmi -f product-service:1.0
 docker rmi -f order-service:1.0
+docker rmi -f payment-service:1.0
---
+++ payment-service/constants.js
@@ -0,0 +1,16 @@
+const constants = {
+  PORT: 5001,
+  API: {
+    PREFIX: "/api/v1",
+  },
+  ACCESS: {
+    ROLES: {
+      ADMIN: "admin",
+      SELLER: "seller",
+      CUSTOMER: "customer",
+      SERVICE: "service",
+    },
+  },
+};
+
+module.exports = constants;
